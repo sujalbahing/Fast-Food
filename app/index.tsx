032a820b86ef04efceb98d21f618a5e5c2a8ee1d
Sujal Rai <sujal.rai147@gmail.com>
2025-12-29 17:36:19 +0545

    Update in Images and Fonts

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import "./global.css"
+import { Text, View } from "react-native";
+ 
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
+        Welcome to my React Native App!
+      </Text>
+    </View>
+  );
+}
